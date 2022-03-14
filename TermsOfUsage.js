5e5dca13b83388c3695159956fa0cf1e3fb435e3
unknown <wojtini777@gmail.com>
2022-03-14 17:05:19 +0100

    Button disable functionality when checkbox is inactive fixed. Tos finished

--- TermsOfUsage.js
+++ TermsOfUsage.js
@@ -19,6 +19,7 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
  
 
 const TermsOfUsage = ({ navigation }) => {
+    const [isDisabled, setIsDisabled] = useState(true);
     const [isSelected, setSelection] = useState(false);
     
     
@@ -30,13 +31,22 @@ const TermsOfUsage = ({ navigation }) => {
         navigation.navigate('HomeTabs');      
     }
     
+    const canBeSubmitted = () => {
+        return isSelected ? setIsDisabled(true) : setIsDisabled(false);
+    };
+
+    const onCheckboxClick = () => {
+        setSelection(!isSelected);
+        return canBeSubmitted();
+    };
+    
 
     useEffect(() => {
         const getData = async () => {
             try {
               const termsReading = await AsyncStorage.getItem('acceptThisTerms')
               if(termsReading != null) {         
-                //navigation.navigate('HomeTabs');   
+                navigation.navigate('HomeTabs');   
               }
             } catch(e) {
               
@@ -45,25 +55,33 @@ const TermsOfUsage = ({ navigation }) => {
           getData();
     },[])
 
+    
+
 
     return (
         <View style={styles.container}>
             <View style={{justifyContent:'center',alignItems:'center',minHeight:'30%', minWidth:100,}}>
-                <Text style={styles.termsText}>Mobile app Terms and Conditions</Text>
+                <Text style={styles.termsText}>CoinApp Terms and Conditions</Text>
+                <View style={{height:400,display:'flex', justifyContent:'center',alignContent:'center',}}>
                 <Text style={styles.termsBigText}>
-                    Clicking button accept you agreed to CoinApp Terms of Servie and Privacy Policy
+                    This app is free to use and share !{"\n"}
                 </Text>
+                <Text style={styles.termsBigText}>
+                    Enjoy!
+                </Text>
+                </View>
+                <View style={{paddingTop:'15%'}}>
                 <View style={{display:'flex',flexDirection:'row',marginTop:'5%',paddingBottom:'5%',}}>
                     <CheckBox
                         value={isSelected}
-                        onValueChange={ setSelection }
+                        onValueChange={onCheckboxClick }
                         style={styles.checkbox}
+                        
                     /> 
                     <Text style={{marginTop:5}}>I have read and accept Terms and Conditions</Text>
                 </View>
-                <View style={{paddingTop:'90%'}}>
-                    <TouchableOpacity style={ isSelected ? styles.acceptButton : styles.disabledButton  } onPress={() => acceptTerms()} disabled={() =>setSelection(!isSelected)}>
-                        <Text style={{fontSize:25,}}>Accept</Text>
+                    <TouchableOpacity style={ isSelected ? styles.acceptButton : styles.disabledButton  } onPress={() => acceptTerms()} disabled={isDisabled}>
+                        <Text style={{fontSize:25, color:'white',}}>Accept</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -91,20 +109,23 @@ const styles = StyleSheet.create({
         fontSize:30,
         textAlign:'center',
         alignItems:'center',
-        paddingTop:3,
+        paddingTop:5,
+        marginTop:20,
         
 
     },
     termsBigText:{
         marginTop:'10%',
-        fontSize:25,
+        fontSize:20,
         justifyContent:'center',
         alignItems:'center',
+        textAlign:'center',
+        
         padding:6,
     },
     acceptButton:{
        
-        height:50,
+        height:65,
         width:350,
         
         justifyContent:'center',
@@ -122,7 +143,7 @@ const styles = StyleSheet.create({
         elevation: 9,
     },
     disabledButton:{
-        height:50,
+        height:65,
         width:350,
         backgroundColor:'#364954',
         justifyContent:'center',
