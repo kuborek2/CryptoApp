fdedac465bf4c4907c70296ab2d36b422dfa761b
unknown <wojtini777@gmail.com>
2022-03-14 14:04:24 +0100

    Button change style on checkbox change but not value of disable to fix . Screen Tos need polsihing not finished yet.

--- TermsOfUsage.js
+++ TermsOfUsage.js
@@ -10,14 +10,20 @@ import {
     Text,
     useColorScheme,
     View,
-
-  } from 'react-native';
+    } from 'react-native';
+  import CheckBox from '@react-native-community/checkbox';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
  
 
 const TermsOfUsage = ({ navigation }) => {
+    const [isSelected, setSelection] = useState(false);
+    
+    
+
+ 
 
     const acceptTerms = async () => {
         AsyncStorage.setItem('acceptThisTerms','true'); 
@@ -30,7 +36,7 @@ const TermsOfUsage = ({ navigation }) => {
             try {
               const termsReading = await AsyncStorage.getItem('acceptThisTerms')
               if(termsReading != null) {         
-                navigation.navigate('HomeTabs');   
+                //navigation.navigate('HomeTabs');   
               }
             } catch(e) {
               
@@ -42,30 +48,43 @@ const TermsOfUsage = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
-            <View>
-                <Text style={styles.termsText}>Terms of Usage</Text>
+            <View style={{justifyContent:'center',alignItems:'center',minHeight:'30%', minWidth:100,}}>
+                <Text style={styles.termsText}>Mobile app Terms and Conditions</Text>
                 <Text style={styles.termsBigText}>
-                Lorep ipmsum Lorep ipmsum
-                Lorep ipmsum Lorep ipmsum
-                Lorep ipmsum Lorep ipmsum
-                Lorep ipmsum Lorep ipmsum
-                Lorep ipmsum Lorep ipmsum
-                Lorep ipmsum Lorep ipmsum
-                <Button title='Zaakceptuj' onPress={() => acceptTerms()}/>
+                    Clicking button accept you agreed to CoinApp Terms of Servie and Privacy Policy
                 </Text>
+                <View style={{display:'flex',flexDirection:'row',marginTop:'5%',paddingBottom:'5%',}}>
+                    <CheckBox
+                        value={isSelected}
+                        onValueChange={ setSelection }
+                        style={styles.checkbox}
+                    /> 
+                    <Text style={{marginTop:5}}>I have read and accept Terms and Conditions</Text>
+                </View>
+                <View style={{paddingTop:'90%'}}>
+                    <TouchableOpacity style={ isSelected ? styles.acceptButton : styles.disabledButton  } onPress={() => acceptTerms()} disabled={() =>setSelection(!isSelected)}>
+                        <Text style={{fontSize:25,}}>Accept</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-    
+               
         </View>
     );
 
 }
 
 
+
+
 const styles = StyleSheet.create({
+
     container:{
+        maxHeight:'100%',
+        maxWidth:'100%',
         display:'flex',
         justifyContent:'center',
         alignItems:'center',
+        
         
     },
     termsText:{
@@ -73,13 +92,53 @@ const styles = StyleSheet.create({
         textAlign:'center',
         alignItems:'center',
         paddingTop:3,
+        
 
     },
     termsBigText:{
+        marginTop:'10%',
         fontSize:25,
         justifyContent:'center',
         alignItems:'center',
-    }
+        padding:6,
+    },
+    acceptButton:{
+       
+        height:50,
+        width:350,
+        
+        justifyContent:'center',
+        alignItems:'center',
+        
+        backgroundColor:'#51bbe9',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+
+        elevation: 9,
+    },
+    disabledButton:{
+        height:50,
+        width:350,
+        backgroundColor:'#364954',
+        justifyContent:'center',
+        alignItems:'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+
+        elevation: 9,
+        
+    },
+
 });
 
 export default TermsOfUsage;
